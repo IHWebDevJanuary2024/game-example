539e7388889d23288453b8ff64422268ef474f3f
Marshall-Bits <kz_@hotmail.es>
2024-07-11 17:25:34 +0200

    add diagonal movement

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let gameLives = 3;
 
 const boardElement = document.querySelector("#game-board");
 
-console.log(boardElement.getBoundingClientRect()) // that's how we get the width and height of an element
+console.log(boardElement.getBoundingClientRect()); // that's how we get the width and height of an element
 
 const boardWidth = boardElement.getBoundingClientRect().width;
 const boardHeight = boardElement.getBoundingClientRect().height;
@@ -22,43 +22,42 @@ setInterval(() => {
 }, 30) 
 */
 
-
 function gameLoop() {
-    if (!game.gameOver) {
-        game.frames++;
-        game.player.crashTest()
-        game.player.move();
-        if (game.frames % enemyCreationFrame === 0) {
-            game.enemies.push(new Enemy(enemyVelocity));
-            console.log(game.enemies);
-        }
-        if (game.frames % scoreIncrementFrame === 0) {
-            game.score++;
-            game.updateScore();
-        }
-        game.enemies.forEach((enemy) => {
-            enemy.move()
-            enemy.checkForBoundaries()
-        })
-        requestAnimationFrame(gameLoop); // we can create an infinite loop without braking it;
-    } else { // IF THE GAME IS OVER
-        gameOverElement.style.display = "block";
-        game.player.element.remove();
-        document.querySelector("#final-score").innerText = game.score;
+  if (!game.gameOver) {
+    game.frames++;
+    game.player.crashTest();
+    game.player.move();
+    if (game.frames % enemyCreationFrame === 0) {
+      game.enemies.push(new Enemy(enemyVelocity));
+      console.log(game.enemies);
     }
+    if (game.frames % scoreIncrementFrame === 0) {
+      game.score++;
+      game.updateScore();
+    }
+    game.enemies.forEach((enemy) => {
+      enemy.move();
+      enemy.checkForBoundaries();
+    });
+    requestAnimationFrame(gameLoop); // we can create an infinite loop without braking it;
+  } else {
+    // IF THE GAME IS OVER
+    gameOverElement.style.display = "block";
+    game.player.element.remove();
+    document.querySelector("#final-score").innerText = game.score;
+  }
 }
 
 requestAnimationFrame(gameLoop);
 
-
 document.addEventListener("keydown", (event) => {
-    if (!game.gameOver) {
-        game.player.setDirection(event.key);
-    }
-})
+  if (!game.gameOver) {
+    game.player.setDirection(event.key);
+  }
+});
 document.addEventListener("keyup", (event) => {
-    console.log("keyup");
-    if (!game.gameOver) {
-        game.player.setDirection("none");
-    }
-})
+  console.log("keyup");
+  if (!game.gameOver) {
+    game.player.deleteDirection(event.key);
+  }
+});
--- player.js
+++ player.js
@@ -1,81 +1,107 @@
 class Player {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.velocity = 5
-        this.direction = "none"
-        this.element = document.querySelector("#player");
-        this.width = this.element.getBoundingClientRect().width;
-        this.height = this.element.getBoundingClientRect().height;
-        this.element.style.left = `${this.x}px`
-        this.element.style.top = `${this.y}px`
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.velocity = 5;
+    this.direction = [];
+    this.element = document.querySelector("#player");
+    this.width = this.element.getBoundingClientRect().width;
+    this.height = this.element.getBoundingClientRect().height;
+    this.element.style.left = `${this.x}px`;
+    this.element.style.top = `${this.y}px`;
+  }
+  setDirection(direction) {
+    if (!this.direction.includes(direction)) {
+      this.direction.push(direction);
     }
-    setDirection(diretction) {
-        this.direction = diretction;
+  }
+  deleteDirection(direction) {
+    const index = this.direction.indexOf(direction);
+    this.direction.splice(index, 1);
+  }
+  move() {
+    const direction = this.direction.join("");
+    switch (direction) {
+      case "ArrowUp":
+        this.y -= this.velocity;
+        break;
+      case "ArrowDown":
+        this.y += this.velocity;
+        break;
+      case "ArrowLeft":
+        this.x -= this.velocity;
+        break;
+      case "ArrowRight":
+        this.x += this.velocity;
+        break;
+      case "ArrowUpArrowRight":
+      case "ArrowRightArrowUp":
+        this.y -= this.velocity;
+        this.x += this.velocity;
+        break;
+      case "ArrowUpArrowLeft":
+      case "ArrowLeftArrowUp":
+        this.y -= this.velocity;
+        this.x -= this.velocity;
+        break;
+      case "ArrowDownArrowRight":
+      case "ArrowRightArrowDown":
+        this.y += this.velocity;
+        this.x += this.velocity;
+        break;
+      case "ArrowDownArrowLeft":
+      case "ArrowLeftArrowDown":
+        this.y += this.velocity;
+        this.x -= this.velocity;
+        break;
+      case "":
+        this.x = this.x;
+        this.y = this.y;
+        break;
     }
-    move() {
-        const direction = this.direction;
-        switch (direction) {
-            case "ArrowUp":
-                this.y -= this.velocity;
-                break;
-            case "ArrowDown":
-                this.y += this.velocity;
-                break;
-            case "ArrowLeft":
-                this.x -= this.velocity;
-                break;
-            case "ArrowRight":
-                this.x += this.velocity;
-                break;
-            case "none":
-                this.x = this.x;
-                this.y = this.y;
-                break;
-        };
 
-        this.checkForBoundaries()
+    this.checkForBoundaries();
 
-        this.element.style.left = `${this.x}px`;
-        this.element.style.top = `${this.y}px`;
+    this.element.style.left = `${this.x}px`;
+    this.element.style.top = `${this.y}px`;
+  }
+  checkForBoundaries() {
+    if (this.x <= 0) {
+      this.x = 0;
     }
-    checkForBoundaries() {
-        if (this.x <= 0) {
-            this.x = 0;
-        }
-        if (this.y <= boardHeight * 0.20) {
-            this.y = boardHeight * 0.20;
-        }
-        if (this.y >= boardHeight * 0.75- this.height) {
-            this.y = boardHeight * 0.75 - this.height;
-        }
-        if (this.x >= boardWidth - this.width) {
-            this.x = boardWidth - this.width;
-        }
+    if (this.y <= boardHeight * 0.2) {
+      this.y = boardHeight * 0.2;
     }
-    crashTest() {
-        game.enemies.forEach((enemy) => {
-            // HUMAN READABLE 😅
-            const enemyLeftEdge = enemy.x;
-            const enemyRightEdge = enemy.x + enemy.width;
-            const enemyTopEdge = enemy.y;
-            const enemyBottomEdge = enemy.y + enemy.height;
+    if (this.y >= boardHeight * 0.75 - this.height) {
+      this.y = boardHeight * 0.75 - this.height;
+    }
+    if (this.x >= boardWidth - this.width) {
+      this.x = boardWidth - this.width;
+    }
+  }
+  crashTest() {
+    game.enemies.forEach((enemy) => {
+      // HUMAN READABLE 😅
+      const enemyLeftEdge = enemy.x;
+      const enemyRightEdge = enemy.x + enemy.width;
+      const enemyTopEdge = enemy.y;
+      const enemyBottomEdge = enemy.y + enemy.height;
 
-            const playerLeftEdge = this.x;
-            const playerRightEdge = this.x + this.width;
-            const playerTopEdge = this.y;
-            const playerBottomEdge = this.y + this.height;
+      const playerLeftEdge = this.x;
+      const playerRightEdge = this.x + this.width;
+      const playerTopEdge = this.y;
+      const playerBottomEdge = this.y + this.height;
 
-            if (
-                playerLeftEdge < enemyRightEdge &&
-                playerRightEdge > enemyLeftEdge &&
-                playerTopEdge < enemyBottomEdge &&
-                playerBottomEdge > enemyTopEdge
-            ) {
-                game.lives -= 1;
-                game.updateLives();
-                enemy.deSpawn();
-            }
-        });
-    }
+      if (
+        playerLeftEdge < enemyRightEdge &&
+        playerRightEdge > enemyLeftEdge &&
+        playerTopEdge < enemyBottomEdge &&
+        playerBottomEdge > enemyTopEdge
+      ) {
+        game.lives -= 1;
+        game.updateLives();
+        enemy.deSpawn();
+      }
+    });
+  }
 }
